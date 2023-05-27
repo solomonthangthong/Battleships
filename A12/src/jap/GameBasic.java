24a9b69b1b2489b06835ef4271eab7e9b70be255
Solomon Thangthong <than0047@algonquinlive.com>
2023-05-27 11:07:17 -0400

    fix: frame.setResizable(false)

--- A12/src/jap/GameBasic.java
+++ A12/src/jap/GameBasic.java
@@ -3,6 +3,7 @@ package jap;
 public class GameBasic {
     public static void main(String[] args) {
         GameFrame frame = new GameFrame();
+        frame.setResizable(false);
         frame.setVisible(true);
     }
 }
