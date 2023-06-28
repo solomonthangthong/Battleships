e85324d25b9ae9b71b1284881e855c12db084111
Solomon Thangthong <than0047@algonquinlive.com>
2023-06-28 12:09:56 -0400

    feat: A22 resources
    feat: A22 resources

---
+++ A22/JAP-A123.bat
@@ -0,0 +1,80 @@
+:: ---------------------------------------------------------------------
+:: JAP COURSE - SCRIPT
+:: ASSIGNMENTS - CST8221 - Spring 2023
+:: ---------------------------------------------------------------------
+:: Begin of Script (Assignments - S23)
+:: ---------------------------------------------------------------------
+
+CLS
+
+:: LOCAL VARIABLES ....................................................
+
+SET JAVAFXDIR=/SOFT/copy/dev/java/javafx/lib
+SET SRCDIR=src
+SET BINDIR=bin
+SET BINOUT=game-javac.out
+SET BINERR=game-javac.err
+SET JARNAME=Game.jar
+SET JAROUT=game-jar.out
+SET JARERR=game-jar.err
+SET DOCDIR=doc
+SET DOCPACK=game
+SET DOCOUT=game-javadoc.out
+SET DOCERR=game-javadoc.err
+SET MAINCLASSSRC=src/game/GameBasic.java
+SET MAINCLASSBIN=game.GameBasic
+SET MODULELIST=javafx.controls,javafx.fxml
+
+@echo off
+
+ECHO "                                                                     "
+ECHO "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+ECHO "@                                                                   @"
+ECHO "@                   #       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  @"
+ECHO "@                  ##       @  A L G O N Q U I N  C O L L E G E  @  @"
+ECHO "@                ##  #      @    JAVA APPLICATION PROGRAMMING    @  @"
+ECHO "@             ###    ##     @        S P R I N G - 2 0 2 3       @  @"
+ECHO "@          ###    ##        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  @"
+ECHO "@        ###    ##                                                  @"
+ECHO "@        ##    ###                 ###                              @"
+ECHO "@         ##    ###                ###                              @"
+ECHO "@           ##    ##               ###   #####  ##     ##  #####    @"
+ECHO "@         (     (      ((((()      ###       ## ###   ###      ##   @"
+ECHO "@     ((((     ((((((((     ()     ###   ######  ###  ##   ######   @"
+ECHO "@        ((                ()      ###  ##   ##   ## ##   ##   ##   @"
+ECHO "@         ((((((((((( ((()         ###   ######    ###     ######   @"
+ECHO "@         ((         ((           ###                               @"
+ECHO "@          (((((((((((                                              @"
+ECHO "@   (((                      ((          /-----------------\        @"
+ECHO "@    ((((((((((((((((((((() ))           | BATTLESHIP GAME |        @"
+ECHO "@         ((((((((((((((((()             \-----------------/        @"
+ECHO "@                                                                   @"
+ECHO "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+ECHO "                                                                     "
+
+:: EXECUTION STEPS  ...................................................
+
+
+ECHO "1. Compiling ......................"
+javac -Xlint -cp ".;%SRCDIR%;%JAVAFXDIR%/*" %MAINCLASSSRC% -d %BINDIR% > %BINOUT% 2> %BINERR%
+
+ECHO "2. Creating Jar ..................."
+cd bin
+jar cvfe %JARNAME% %MAINCLASSBIN% . > %JAROUT% 2> %JARERR%
+
+ECHO "3. Creating Javadoc ..............."
+cd ..
+javadoc -cp ".;%BINDIR%;%JAVAFXDIR%" --module-path "%JAVAFXDIR%" --add-modules %MODULELIST% -d %DOCDIR% -sourcepath %SRCDIR% -subpackages %DOCPACK% > %DOCOUT% 2> %DOCERR%
+
+cd bin
+ECHO "4. Running Jar ...................."
+start java --module-path "%JAVAFXDIR%" --add-modules %MODULELIST% -jar %JARNAME%
+cd ..
+
+ECHO "[END OF SCRIPT -------------------]"
+ECHO "                                   "
+@echo on
+
+:: ---------------------------------------------------------------------
+:: End of Script (Assignments - S23)
+:: ---------------------------------------------------------------------
