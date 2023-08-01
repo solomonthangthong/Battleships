6beecf6b25fadeb5fc211954f425c137fb8f3515
Solomon Thangthong <than0047@algonquinlive.com>
2023-08-01 18:19:16 -0400

    Line separator
    Move files into A32
    
    Move files into A32
    
    Comment out HIT MISS sound to avoid crash

---
+++ A32/src/jap/GameBasic.java
@@ -0,0 +1,37 @@
+/**
+ * File name: GameBasic.java
+ * Identification: Andrew Lorimer 041056170, Solomon Thangthong 041023691
+ * Course: CST 8221 - JAP, Lab Section: 301
+ * Professor: Paulo Sousa
+ * Date: 06/04/2023
+ * Compiler: Intellij IDEA 2023.1.1 (Community Edition)
+ * Purpose: Main entry point for Batteship
+ */
+package jap;
+
+/**
+ * Class Name: GameBasic
+ * Method List: main
+ * Purpose: Host file for main method and entry
+ *
+ * @author Andrew Lorimer, Solomon Thangthong
+ * @version 2.0
+ * @since 11.0.19
+ */
+public class GameBasic {
+    /**
+     * Method Name: main
+     * Purpose: The main entry point for the Battleship application.
+     *
+     * @param args the command-line arguments passed to the program
+     */
+    public static void main(String[] args) {
+        GameModel gameModel = new GameModel();
+        GameView gameView = new GameView();
+        GameController gameController = new GameController(gameModel, gameView);
+
+        gameView.setResizable(false);
+        gameView.setVisible(true);
+
+    }
+}
