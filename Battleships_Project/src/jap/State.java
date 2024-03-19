7c4f953e958dd0525b90565756a72e454cec4de3
Solomon Thangthong <than0047@algonquinlive.com>
2024-03-19 08:39:19 -0400

    Refactor Refactor

---
+++ Battleships_Project/src/jap/State.java
@@ -0,0 +1,17 @@
+package jap;
+
+/**
+ * Class Name: State
+ * Method List:
+ * Constants List:
+ * Represents the possible states of a button entity.
+ *
+ * @author Andrew Lorimer, Solomon Thangthong
+ * @version 1.0
+ * @since 11.0.19
+ */
+public enum State {
+    DEFAULT,
+    HIT,
+    MISS
+}
